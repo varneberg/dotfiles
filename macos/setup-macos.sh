6ecb402e23b9510e0545be190ac5684580f332e9
varneberg <varneberg@gmail.com>
2023-05-10 12:46:00 +0200

    brew tap list and spicetify

--- macos/setup-macos.sh
+++ macos/setup-macos.sh
@@ -5,10 +5,7 @@ then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-for i in $(cat macos/brewlist.txt)
-do
-				brew install $i
-done
+brew install $(cat macos/brewlist.txt)
 
 # Configs
 #cp -r config ~/.config
@@ -16,3 +13,12 @@ done
 # Vim
 ## Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Spicetify
+# Install
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
+
+## Spicetify themes
+git clone --depth=1 https://github.com/spicetify/spicetify-themes.git 
+cd spicetify-themes
+cp -r * ~/.config/spicetify/Themes
