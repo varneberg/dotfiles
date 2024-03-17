c84601db1891090e0271a05f8d1d74fce145f110
Vegard Arneberg <varneberg@gmail.com>
2024-03-17 17:25:44 +0100

    oh my zsh set

--- setup-macos.sh
+++ setup-macos.sh
@@ -4,14 +4,17 @@ if !(type brew &>/dev/null)
 then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+brew install $(cat macos/brewlist.txt)
+
+# ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cp home/.zsh_alias ~/
 cp home/.zshenv ~/
 cp home/.zshrc ~/
-cp home.vimrc ~/
 
-brew install $(cat macos/brewlist.txt)
-#xargs brew install --cask < macos/brew_list.txt
 
 # Configs
 cp -r config/*  ~/.config/
@@ -19,5 +22,6 @@ cp -r config/*  ~/.config/
 # Vim
 ## Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp home.vimrc ~/
 
 
